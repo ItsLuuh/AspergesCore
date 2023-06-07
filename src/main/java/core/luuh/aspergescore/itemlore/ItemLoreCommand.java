c195926d0898d93948eb5963f31374561e938b6f
ItsLuuh <luuh2640@gmail.com>
2023-06-07 21:26:00 +0200

    Commit to push everything on master

--- src/main/java/core/luuh/aspergescore/itemlore/ItemLoreCommand.java
+++ src/main/java/core/luuh/aspergescore/itemlore/ItemLoreCommand.java
@@ -2,10 +2,14 @@ package core.luuh.aspergescore.itemlore;
 
 import core.luuh.aspergescore.AspergesCore;
 import core.luuh.verioncore.VerionAPIManager;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 public class ItemLoreCommand implements CommandExecutor {
@@ -27,22 +31,84 @@ public class ItemLoreCommand implements CommandExecutor {
 
             //itemlore rename (Name)
             //itemlore addlore (Text)
-            //itemlore setloreline (Line) (Text)
-            //itemlore removeloreline (Line)
-            //itemlore addloreline (Line) (Text)
+            //itemlore loreline set (Line) (Text)
+            //itemlore loreline remove (Line)
+            //itemlore loreline add (Line) (Text)
             //itemlore resetlore
-            //itemlore resetname
             //itemlore glow
             //itemlore removeglow
             //itemlore hidenchants
+            //itemlore nbt add (Key) (Value)
+            //itemlore nbt remove (Value)
+            //itemlore nbt get (Value)
+            //itemlore nbt all
 
             Player player = (Player) sender;
 
-            if(args.length == 0){
 
-                player.sendMessage("");
+            if(player.getInventory().getItemInMainHand().equals(Material.AIR)){
 
+                VerionAPIManager.logConsole("test");
+
+            } else {
+                ItemStack item = player.getInventory().getItemInMainHand();
+                if (args.length == 0) {
+
+
+                } else if (args.length == 1) {
+
+                    switch (args[0]) {
+
+                        case "resetlore":
+
+                            item.getItemMeta().lore(null);
+
+                            break;
+                        case "glow":
+
+                            VerionAPIManager.setEnchanted(item, true);
+
+                            break;
+                        case "removeglow":
+
+                            VerionAPIManager.setEnchanted(item, false);
+
+                            break;
+                        case "hidenchants":
+                        case "hideenchants":
+
+                            ItemMeta itemMeta = item.getItemMeta();
+                            itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                            item.setItemMeta(itemMeta);
+
+                            break;
+
+                    }
+
+
+                } else if (args.length == 2) {
+
+                    if (args[0].equalsIgnoreCase("rename")) {
+
+                        String itemName = args[0];
+                        item.getItemMeta().setDisplayName(itemName);
+
+
+                    } else if (args[0].equalsIgnoreCase("addlore")) {
+
+
+                    } else if (args[0].equalsIgnoreCase("removeloreline")) {
+
+
+                    } else if (args[0].equalsIgnoreCase("nbt")) {
+
+
+
+                    }
+
+                }
             }
+
 
         }
 
